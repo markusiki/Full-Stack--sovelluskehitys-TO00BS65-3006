abead3a4fc6bbf9bcd26a50d3d4bc5ec9303d094
Markus <markus.kitunen@outlook.com>
2024-03-13 10:58:55 +0200

    comments added

--- projektitehtava_1/index.js
+++ projektitehtava_1/index.js
@@ -8,8 +8,10 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json())
 
+// Landing page (route '/')
 app.use(express.static('public'))
 
+// Sends all saved messages as a reponse
 app.get('/guestbook', (req, res) => {
   const data = require('./JSON_Guestbook_data.json')
 
@@ -29,15 +31,17 @@ app.get('/guestbook', (req, res) => {
   res.send(results)
 })
 
+// Sends the newmessage html form as a response
 app.get('/newmessage', (req, res) => {
   res.sendFile(__dirname + '/public/newmessage.html')
 })
 
+// Receives the newmessage form and saves it to the database
 app.post('/newmessage', (req, res) => {
   const data = require('./JSON_Guestbook_data.json')
   const body = req.body
 
-  const newmessage = {
+  const newMessage = {
     id: data.length + 1,
     username: body.username,
     country: body.country,
@@ -45,25 +49,27 @@ app.post('/newmessage', (req, res) => {
     message: body.message,
   }
 
-  data.push(newmessage)
+  data.push(newMessage)
 
   jsonStr = JSON.stringify(data)
 
   fs.writeFile('JSON_Guestbook_data.json', jsonStr, (err) => {
     if (err) throw err
   })
-  res.send(`New message: ${newmessage.message}, saved successfully!`)
+  res.send(`New message: ${newMessage.message}, saved successfully!`)
 })
 
+// Sends the ajaxmessage html form as a response
 app.get('/ajaxmessage', (req, res) => {
   res.sendFile(__dirname + '/public/ajaxmessage.html')
 })
 
+// Receives the ajaxmessage form data, saves it to the database and sends all messages as a response
 app.post('/ajaxmessage', (req, res) => {
   const data = require('./JSON_Guestbook_data.json')
   const body = req.body
 
-  const newmessage = {
+  const newMessage = {
     id: data.length + 1,
     username: body.username,
     country: body.country,
@@ -71,7 +77,7 @@ app.post('/ajaxmessage', (req, res) => {
     message: body.message,
   }
 
-  data.push(newmessage)
+  data.push(newMessage)
 
   jsonStr = JSON.stringify(data)
 
