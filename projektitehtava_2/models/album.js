dab5ecd3bbe1d2049c9984aa3f0a77e8b13953e0
Markus <markus.kitunen@outlook.com>
2024-04-08 13:58:34 +0300

    toJSON settings changed not to return id

--- projektitehtava_2/models/album.js
+++ projektitehtava_2/models/album.js
@@ -19,8 +19,6 @@ const albumSchema = mongoose.Schema({
 
 albumSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
     delete returnedObject.__v
   },
 })
--- projektitehtava_2/models/artist.js
+++ projektitehtava_2/models/artist.js
@@ -21,8 +21,6 @@ const artistSchema = mongoose.Schema({
 
 artistSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
     delete returnedObject.__v
   },
 })
