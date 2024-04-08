cd8720987b1c692bc01c3bed725055bbe1176131
Markus <markus.kitunen@outlook.com>
2024-04-08 12:46:02 +0300

    /aou/add/ route implemented

--- projektitehtava_2/controllers/playlist.js
+++ projektitehtava_2/controllers/playlist.js
@@ -7,6 +7,11 @@ playlistRouter.get('/getall', async (req, res) => {
   const playlist = await Song.find({})
     .populate('album', { title: 1, date: 1 })
     .populate('artist', { name: 1 })
+
+  if (!playlist) {
+    res.status(200).json({ message: 'Playlist is empty' })
+    return
+  }
   res.json(playlist)
 })
 
@@ -14,43 +19,65 @@ playlistRouter.post('/add', async (req, res) => {
   try {
     const body = req.body
 
-    const song = await Song.findOne({ title: body.title })
+    const song = await Song.findOne({
+      title: { $regex: new RegExp(body.title, 'i') },
+    })
+    const existingArtist = await Artist.findOne({
+      name: { $regex: new RegExp(body.artist, 'i') },
+    })
     console.log('song', song)
-    if (song) {
+    if (song && existingArtist) {
       res.status(400).json({ message: 'Song already exists' })
       return
     }
+    const getArtist = () => {
+      if (!existingArtist) {
+        const newArtist = Artist.create({ name: body.artist })
+        return newArtist
+      }
+      return existingArtist
+    }
+    const artist = await getArtist()
+    console.log('artist', artist)
 
-    const artist = await Artist.findOneAndUpdate({
-      conditions: { name: body.artist },
-      options: { new: true, upsert: true },
+    const existingAlbum = await Album.findOne({
+      title: { $regex: new RegExp(body.album, 'i') },
     })
-    console.log(artist)
-    const album = await Album.findOneAndUpdate({
-      conditions: { title: body.album },
-      options: { new: true, upsert: true },
-    })
-    console.log(album)
+    const getAlbum = () => {
+      if (!existingAlbum) {
+        console.log('newAlbum Created')
+        const newAlbum = Album.create({ title: body.album })
+        return newAlbum
+      }
+      console.log('Existing album used')
+      return existingAlbum
+    }
+
+    const album = await getAlbum()
+    console.log('album', album)
 
     const newSong = new Song({
       title: body.title,
-      artist: artist,
+      artist: artist._id,
       genre: body.genre,
-      album: album,
+      album: album._id,
       year: body.year,
     })
 
-    const savedSong = newSong.save()
-    if (!artist.albums.includes(body.album)) {
-      artist.albums.concat(album._id)
+    const savedSong = await newSong.save()
+    console.log('savedSong', savedSong)
+    if (!artist.albums.includes(album._id)) {
+      artist.albums = artist.albums.concat(album._id)
     }
-    artist.songs.concat(savedSong._id)
+    artist.songs = artist.songs.concat(savedSong._id)
     await artist.save()
     if (!album.artist) {
-      album.artist = body.artist
+      album.artist = artist
     }
+    album.songs = album.songs.concat(savedSong._id)
+    await album.save()
 
-    res.status(200).json(savedSong.toJSON())
+    res.status(200).json(savedSong)
   } catch (error) {
     console.log(error)
     res.status(400).json()
