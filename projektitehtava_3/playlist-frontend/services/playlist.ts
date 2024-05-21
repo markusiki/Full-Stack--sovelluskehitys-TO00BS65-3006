d2b9a6b5322fa0a189286265b414355ae008a8b3
Markus <markus.kitunen@outlook.com>
2024-05-21 18:23:21 +0300

    getByName and addSong functions added

--- projektitehtava_3/playlist-frontend/services/playlist.ts
+++ projektitehtava_3/playlist-frontend/services/playlist.ts
@@ -1,3 +1,4 @@
+import { ISong } from "@/interfaces";
 import axios from "axios";
 
 
@@ -6,6 +7,18 @@ const getAll = async () => {
   return response.data
 }
 
-const playlistServices = { getAll }
+const getByName = async (songName: string) => {
+  const response = await axios.get(`api/getbyname/${songName}`)
+  return response.data
+  console.log(response)
+}
+
+const addSong = async (song: ISong) => {
+  const response = await axios.post('api/add', song)
+  console.log(response)
+  return response
+}
+
+const playlistServices = { getAll, getByName, addSong }
 
 export default playlistServices
